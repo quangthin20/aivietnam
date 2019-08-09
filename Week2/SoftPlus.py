33659d68a565e06badd3a982dae2bcdf5fbe08e6
= <=>
2019-08-09 15:41:52 +0700

    Sua file

---
+++ Week2/SoftPlus.py
@@ -0,0 +1,17 @@
+# Hàm SoftPlus
+
+# @author: Quang Thin Tran
+
+dataset = [1,4,-6,9,-3]
+alpha = 0.1
+def PReLU(data,alpha):
+    result = []
+    for i in data:
+        if i<0:
+            result.append(round(alpha*int(i),3))
+        else:
+            result.append(i)
+    return result
+
+result = PReLU(dataset,alpha)
+print(result)
